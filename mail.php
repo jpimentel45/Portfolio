57a192edfdb4fb02ca9024b99f72fa909f190ffa
Juan Alberto Pimentel JR <jpimentel45@gmail.com>
2019-05-14 16:46:54 -0700

    modifies js, html, and php for some reason youtube wont click on mobile, working on desktop

--- mail.php
+++ mail.php
@@ -2,35 +2,20 @@
 $name = $_POST['name'];
 $email = $_POST['email'];
 $message = $_POST['message'];
-$subject = $_POST['subject'];
-header('Content-Type: application/json');
-if ($name === ''){
-print json_encode(array('message' => 'Name cannot be empty', 'code' => 0));
-exit();
-}
-if ($email === ''){
-print json_encode(array('message' => 'Email cannot be empty', 'code' => 0));
-exit();
-} else {
-if (!filter_var($email, FILTER_VALIDATE_EMAIL)){
-print json_encode(array('message' => 'Email format invalid.', 'code' => 0));
-exit();
-}
-}
-if ($subject === ''){
-print json_encode(array('message' => 'Subject cannot be empty', 'code' => 0));
-exit();
-}
-if ($message === ''){
-print json_encode(array('message' => 'Message cannot be empty', 'code' => 0));
-exit();
-}
-$content="From: $name \nEmail: $email \nMessage: $message";
-$recipient = "jpimentel45@gmail.com";
-$mailheader = "From: $email \r\n";
-mail($recipient, $subject, $content, $mailheader) or die("Error!");
-print json_encode(array('message' => 'Email successfully sent!', 'code' => 1));
-exit();
+
+$email_from = "jpimentel45@gmail.com";
+
+$email_subject = "New Form Submission";
+
+$email_body = "User Name: $name.\n".
+"User Emal: $visitor.email.\n".
+"User Message: $message.\n";
+
+$to = "jpimentel45@gmail.com"
+$headers ="From: $email_from\r\n";
+$headers .= "Reply-To: $visitor_email\r\n";
+mail($to,$email_subject,$email_body,$headers);
+heder("Location: index.html");
 ?>
 
 
