4c16dba416cdfc63c7d0e3a2d52370186121c99a
Juan Alberto Pimentel JR <jpimentel45@gmail.com>
2019-05-14 16:37:09 -0700

    added mail.php, updated contact form will test live, added youtube contaier with playlist api

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
   //display a container containing welcome message or something cool
   //once they select options on nav bar ".on click listener", show the appropriate container
   $(".container").hide();
+
   $("#welcome").show();
   //for phone add an on click event for .dropdown, and pust display:show for .dropdown-content
   $(".dropdown").click(function() {
@@ -23,6 +24,11 @@ $(document).ready(function() {
   $("#port").click(function() {
     $(".container").hide();
     show: $("#portfolio").show();
+  });
+  //else if youtube clicked
+  $("#yubby").click(function() {
+    $(".container").hide();
+    show: $("#tubez").show();
   });
   // else if #contact clicked hide: $(".container").hide();, main page we'll implement later, and show: $("#contact").show();
   $("#contact").click(function() {
@@ -71,4 +77,112 @@ $(document).ready(function() {
   createProject();
   //code for portfolio
   //create a div containing image, link, and description about project
+
+  //code for welcome
+  var key = "AIzaSyBKWuAXHLyKPwd7Hi-UrN_tN2pK-Da_suQ";
+  var playlistId = "PLvDeMMtitSeveZ0CgBKZR9quXkKfukoHm";
+  var URL = "https://www.googleapis.com/youtube/v3/playlistItems";
+
+  var options = {
+    part: "snippet",
+    key: key,
+    maxResults: 20,
+    playlistId: playlistId
+  };
+  loadVids();
+  function loadVids() {
+    $.getJSON(URL, options, function(data) {
+      var id = data.items[0].snippet.resourceId.videoId;
+      mainVid(id);
+      resultsLoop(data);
+    });
+  }
+  function mainVid(id) {
+    $("#video").html(`
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/${id}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+  `);
+  }
+  function resultsLoop(data) {
+    $.each(data.items, function(i, item) {
+      var thumb = item.snippet.thumbnails.medium.url;
+      var title = item.snippet.title;
+      var desc = item.snippet.description.substring(0, 100);
+      var vid = item.snippet.resourceId.videoId;
+
+      $("main").append(`
+        <article class="item" data-key="${vid}">
+
+          <img src="${thumb}" alt="" class="thumb">
+          <div class="details">
+            <h4 class='tubs'>${title}</h4>
+            <p class='tubs grey'>${desc}</p>
+          </div>
+
+        </article>
+      `);
+    });
+  }
+  // CLICK EVENT
+  $("main").on("click", "article", function() {
+    var id = $(this).attr("data-key");
+    mainVid(id);
+  });
+
+  //contact form
+  function validateForm() {
+    var name = document.getElementById("name").value;
+    if (name == "") {
+      document.getElementById("status").innerHTML = "Name cannot be empty";
+      return false;
+    }
+    var email = document.getElementById("email").value;
+    if (email == "") {
+      document.getElementById("status").innerHTML = "Email cannot be empty";
+      return false;
+    } else {
+      var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      if (!re.test(email)) {
+        document.getElementById("status").innerHTML = "Email format invalid";
+        return false;
+      }
+    }
+    var subject = document.getElementById("subject").value;
+    if (subject == "") {
+      document.getElementById("status").innerHTML = "Subject cannot be empty";
+      return false;
+    }
+    var message = document.getElementById("message").value;
+    if (message == "") {
+      document.getElementById("status").innerHTML = "Message cannot be empty";
+      return false;
+    }
+    document.getElementById("status").innerHTML = "Sending...";
+    document.getElementById("contact-form").submit();
+  }
+  //send form without reload
+  document.getElementById("status").innerHTML = "Sending...";
+  formData = {
+    name: $("input[name=name]").val(),
+    email: $("input[name=email]").val(),
+    subject: $("input[name=subject]").val(),
+    message: $("textarea[name=message]").val()
+  };
+
+  $.ajax({
+    url: "mail.php",
+    type: "POST",
+    data: formData,
+    success: function(data, textStatus, jqXHR) {
+      $("#status").text(data.message);
+      if (data.code)
+        //If mail was sent successfully, reset the form.
+        $("#contact-form")
+          .closest("form")
+          .find("input[type=text], textarea")
+          .val("");
+    },
+    error: function(jqXHR, textStatus, errorThrown) {
+      $("#status").text(jqXHR);
+    }
+  });
 });
