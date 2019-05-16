2114eae13bccc6a986e0c6815629f2bf8572e3b8
Juan Alberto Pimentel JR <jpimentel45@gmail.com>
2019-05-16 06:55:53 -0700

    created sperate div for text,desc, link want to hide that div, and show once user hovers, clicks on project image, adjust margin on created fiv stored inside img div

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,6 +9,7 @@ $(document).ready(function() {
   $(".dropdown").click(function() {
     $(".dropdown-content").toggle();
   });
+
   // add click event for .dropdown-content
   //click for #wel
   $("#wel").click(function() {
@@ -35,27 +36,31 @@ $(document).ready(function() {
     $(".container").hide();
     show: $("#cont").show();
   });
-
+  //code for portfolio
   var projects = [
     {
       link: "https://jpimentel45.github.io/Giftastic/",
       img: "assets/images/giftstic.png",
-      text: "   Giftastic"
+      text: "   Giftastic",
+      description: "Blah Blah Blah"
     },
     {
       link: "https://jpimentel45.github.io/TrainSchedule/train",
       img: "assets/images/train.png",
-      text: "Train Schedule"
+      text: "Train Schedule",
+      description: "Blah Blah Blah"
     },
     {
       link: "https://jpimentel45.github.io/TriviaGame/",
       img: "assets/images/trivia.png",
-      text: "Trivia Game"
+      text: "Trivia Game",
+      description: "Blah Blah Blah"
     },
     {
       link: "https://jpimentel45.github.io/Psychic_Game/",
       img: "assets/images/psychic.png",
-      text: "Psychic Game"
+      text: "Psychic Game",
+      description: "Blah Blah Blah"
     }
   ];
   var port = '<div class="wrapper">';
@@ -65,23 +70,49 @@ $(document).ready(function() {
     //create array to hold start <option> tags
     var newProjects = [];
     for (var i = 0; i < projects.length; i++) {
+      var projectDiv = $("<div id='show'>");
       var newOption = $("<a>");
       var img = $("<img src='' id='portImg'>");
       img.attr("src", projects[i].img);
       var text = $('<p id="txt">');
       text.text(projects[i].text);
+      var desc = $("<p id = 'description'>");
+      desc.text(projects[i].description);
       newOption.attr("href", projects[i].link); // add [i] here to index element
       // newOption.text(projects[i].text); // add [i] here to index element
+      //create div for text, desc
+      // append text, desc to that div
+      //append to projectDiv after appending newOption, then just leave projectDiv
+      //to be pushed to newProjects
+      //css use simila to dropdown where txt,desc div is hidden
+      //shown on hover, also add on click to toggle on js for phones
+      //give each div their own id to be able to target ex div#imgDiv div#hiddenDiv
+      //on media query for l phone remove .hide for div#hide and set to show
+      var hideDiv = $("<div id='hide'>");
+      //try this
+
+      //hideDiv.append(text);
+      // hideDiv.append(desc);
+      projectDiv.append(img);
+      //newOption.append(img);
       newOption.append(text);
-      newOption.append(img);
-      newProjects.push(newOption); // add new option to the array
+      newOption.append(desc);
+      hideDiv.append(newOption);
+      // newOption.append(text);
+      // newOption.append(desc);
+      projectDiv.append(img);
+      //projectDiv.append(newOption);
+      projectDiv.append(hideDiv);
+      newProjects.push(projectDiv); // add new option to the array
     }
     //appending=expensive, keep array of elements and then use .html to add array to <select>
     $(".wrapper").html(newProjects); // once loop is complete, add the array of elements to the DOM
   }
   createProject();
-  //code for portfolio
-  //create a div containing image, link, and description about project
+  //only toggles first pctures hide div not on others
+  $("#show").click(function() {
+    $("#hide").toggle();
+  });
 
   //code for welcome
   var key = "AIzaSyBKWuAXHLyKPwd7Hi-UrN_tN2pK-Da_suQ";
